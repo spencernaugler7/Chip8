16c4af79bbbdd155b76323834045e03616c4be1f
Spencer Naugler <spencernaugler7@gmail.com>
2023-11-05 18:14:05 -0500

    Quad drawn To screen.

--- Chip8/Program.cs
+++ Chip8/Program.cs
@@ -1,66 +1,33 @@
-﻿
-// Memory: CHIP-8 has direct access to up to 4 kilobytes of RAM
-// Display: 64 x 32 pixels (or 128 x 64 for SUPER-CHIP) monochrome, ie. black or white
-// A program counter, often called just “PC”, which points at the current instruction in memory
-// One 16-bit index register called “I” which is used to point at locations in memory
-// A stack for 16-bit addresses, which is used to call subroutines/functions and return from them
-// An 8-bit delay timer which is decremented at a rate of 60 Hz (60 times per second) until it reaches 0
-// An 8-bit sound timer which functions like the delay timer, but which also gives off a beeping sound as long as it’s not 0
-// 16 8-bit (one byte) general-purpose variable registers numbered 0 through F hexadecimal, ie. 0 through 15 in decimal, called V0 through VF
-// VF is also used as a flag register; many instructions will set it to either 1 or 0 based on some rule, for example using it as a carry flag
-
-// each instruction is two bytes
-// chip 8 instruction layout (nibble = half a byte)
-// 
-// X: The second nibble. Used to look up one of the 16 registers (VX) from V0 through VF.
-// Y: The third nibble. Also used to look up one of the 16 registers (VY) from V0 through VF.
-// N: The fourth nibble. A 4-bit number.
-
-using System.Buffers.Binary;
+﻿using System.Buffers.Binary;
 using System.Drawing;
-using System.Runtime.InteropServices.JavaScript;
+using DotNext.Collections.Generic;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
 using Silk.NET.OpenGL;
 using Window = Silk.NET.Windowing.Window;
 
 namespace Chip8;
-
+// section chip8
 public class Chip8
 {
-    private static GL _gl;
-    private const int ProgramLoadOffset = 0x200; // start of user data everything before this address is font data
+    // gl variables
+    // Todo move gl concerns into custom window service
+    private static GL gl;
+    private uint vao; // vertex array object for gl
+    private uint vbo; // vertex buffer object for gl
+    private uint ebo; // element buffer object for gl
+    private static uint glProgram;
+    
+    private const int FontOffset = 0x200; // start of user data everything before this address is font data
     private const double Chip8InstructionFrequency = 1 / 700D;
-    private double _emuTimingCounter = 0; // used to match the run cycle of every time this hits 14 ms parse a new instruction
+    private double emuTimingCounter = 0; // used to match the run cycle of every time this hits 14 ms parse a new instruction
     private readonly IWindow window;
-    private readonly byte[] memory = new byte[4096];
-    private int pc = ProgramLoadOffset;
+    private byte[] memory = new byte[4096];
+    private int pc = FontOffset;
     private ushort iRegister = 0;
     private Stack<short> addressStack = new();
     private byte delayTimer = Byte.MinValue;
     private byte soundTimer = Byte.MinValue;
-    private static readonly byte[,] Fonts = new byte[16,5]
-    {
-        { 0xF0, 0x90, 0x90, 0x90, 0xF0 }, // 0
-        { 0x20, 0x60, 0x20, 0x20, 0x70 }, // 1
-        { 0xF0, 0x10, 0xF0, 0x80, 0xF0 }, // 2
-        { 0xF0, 0x10, 0xF0, 0x10, 0xF0 }, // 3
-        { 0x90, 0x90, 0xF0, 0x10, 0x10 }, // 4
-        { 0xF0, 0x80, 0xF0, 0x10, 0xF0 }, // 5
-        { 0xF0, 0x80, 0xF0, 0x90, 0xF0 }, // 6
-        { 0xF0, 0x10, 0x20, 0x40, 0x40 }, // 7
-        { 0xF0, 0x90, 0xF0, 0x90, 0xF0 }, // 8
-        { 0xF0, 0x90, 0xF0, 0x10, 0xF0 }, // 9
-        { 0xF0, 0x90, 0xF0, 0x90, 0x90 }, // A
-        { 0xE0, 0x90, 0xE0, 0x90, 0xE0 }, // B
-        { 0xF0, 0x80, 0x80, 0x80, 0xF0 }, // C
-        { 0xE0, 0x90, 0x90, 0x90, 0xE0 }, // D
-        { 0xF0, 0x80, 0xF0, 0x80, 0xF0 }, // E
-        { 0xF0, 0x80, 0xF0, 0x80, 0x80 }  // F
-    };
-    
-    // how the fuck are the timers represented
-    
     // chip 8 variable registers
     private Dictionary<ushort, byte> _chip8RegisterDict = new()
     {
@@ -74,7 +41,6 @@ public class Chip8
         {7, byte.MinValue},
         {8, byte.MinValue},
         {9, byte.MinValue},
-        {10, byte.MinValue},
         {0xA, byte.MinValue},
         {0xB, byte.MinValue},
         {0xC, byte.MinValue},
@@ -83,14 +49,17 @@ public class Chip8
         {0xF, byte.MinValue},
     };
 
-    private void OnRender(double deltaTime)
+    private unsafe void OnRender(double deltaTime)
     {
-        _emuTimingCounter += deltaTime;
-        if (_emuTimingCounter >= Chip8InstructionFrequency)
-        {
-            Chip8MainLoop();
-            _emuTimingCounter = 0;
-        }
+        gl.BindVertexArray(vao);
+        gl.UseProgram(glProgram);
+        gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*) 0);
+        // emuTimingCounter += deltaTime;
+        // if (emuTimingCounter >= Chip8InstructionFrequency)
+        // {
+        //     Chip8MainLoop();
+        //     emuTimingCounter = 0;
+        // }
     }
 
     /// <summary>
@@ -99,12 +68,12 @@ public class Chip8
     /// </summary>
     private void Chip8MainLoop()
     {
-        ushort currentChip8Instruction = BinaryPrimitives.ReadUInt16BigEndian(new[]{memory[pc] , memory[pc + 1]});
+        ushort currentChip8Instruction = BinaryPrimitives.ReadUInt16BigEndian(new[]{memory[FontOffset + pc] , memory[FontOffset + pc + 1]});
         int opcode = ExtractFromShort(currentChip8Instruction, 0, 4); // opcode is in the first nibble (half-byte)
         switch (opcode)
         {
             case 0x0: // chip 8 clear instruction
-                _gl.ClearColor(Color.Black);
+                gl.ClearColor(Color.Black);
                 break;
             case 0x1: // chip 8 jump instruction, jump destination is all the data after the opcode (12 bits long)
                 int dest = ExtractFromShort(currentChip8Instruction, 4, 12); //  
@@ -151,27 +120,124 @@ public class Chip8
         return clone;
     }
 
-    private void Onload()
+    // section Onload
+    private unsafe void Onload()
     {
-        _gl = window.CreateOpenGL();
-        _gl.ClearColor(Color.Black);
+        float[] vertices =
+        {
+            0.5f, 0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f, 0.5f, 0.0f
+        }; // draws a basic quad to the screen
+        
+        uint[] indices =
+        {
+            0u, 1u, 3u,
+            1u, 2u, 3u
+        };
+        
+        gl = window.CreateOpenGL();
+        vao = gl.GenVertexArray();
+        gl.BindVertexArray(vao);
+        
+        vbo = gl.GenBuffer();
+        gl.BindBuffer(BufferTargetARB.ArrayBuffer, vbo);
+        fixed (float* buff = vertices)
+            gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buff, BufferUsageARB.StaticDraw);
+
+        ebo = gl.GenBuffer();
+        gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, ebo);
+        fixed (uint* buff = indices)
+            gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (indices.Length * sizeof(uint)), buff, BufferUsageARB.StaticDraw);
+
+        const string vertexShaderCode =
+            """
+            # version 330 core
+            layout (location = 0) in vec3 aPosition;
+            void main()
+            {
+                gl_Position = vec4(aPosition, 1.0);
+            }
+            """;
+        const string fragmentShaderCode =
+            """
+            # version 330 core
+            out vec4 out_color;
+            void main()
+            {
+                out_color = vec4(1.0, 0.5, 0.2, 1.0);
+            }
+            """;
+        uint vertexShader = gl.CreateShader(ShaderType.VertexShader);
+        gl.ShaderSource(vertexShader, vertexShaderCode);
+
+        uint fragmentShader = gl.CreateShader(ShaderType.FragmentShader);
+        gl.ShaderSource(fragmentShader, fragmentShaderCode);
+        
+        gl.CompileShader(vertexShader);
+        gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+        if (vStatus != (int)GLEnum.True)
+            throw new Exception("Vertex shader failed to compile" + gl.GetShaderInfoLog(vertexShader));
+
+        gl.CompileShader(fragmentShader);
+        gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
+        if (fStatus != (int)GLEnum.True)
+            throw new Exception("Fragment shader failed to compile" + gl.GetShaderInfoLog(fragmentShader));
+        
+        glProgram = gl.CreateProgram();
+        
+        gl.AttachShader(glProgram, vertexShader);
+        gl.AttachShader(glProgram, fragmentShader);
+        
+        gl.LinkProgram(glProgram);
+        gl.GetProgram(glProgram, ProgramPropertyARB.LinkStatus, out int lStatus);
+        if (lStatus != (int)GLEnum.True)
+            throw new Exception("Program failed to link: " + gl.GetProgramInfoLog(glProgram));
+        
+        gl.DetachShader(glProgram, vertexShader);
+        gl.DetachShader(glProgram, fragmentShader);
+        gl.DeleteShader(vertexShader);
+        gl.DeleteShader(fragmentShader);
+
+        const uint positionLoc = 0;
+        gl.EnableVertexAttribArray(positionLoc);
+        gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*) 0);
+        
+        gl.BindVertexArray(0);
+        gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
+        gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+        
+        //finally draw shit
+        
     }
     
-    private Chip8(IWindow window, byte[] instructions)
+    private Chip8 (IWindow window, byte[] instructions)
     {
+        byte[] fonts = {
+             0xF0, 0x90, 0x90, 0x90, 0xF0 , // 0
+             0x20, 0x60, 0x20, 0x20, 0x70 , // 1
+             0xF0, 0x10, 0xF0, 0x80, 0xF0 , // 2
+             0xF0, 0x10, 0xF0, 0x10, 0xF0 , // 3
+             0x90, 0x90, 0xF0, 0x10, 0x10 , // 4
+             0xF0, 0x80, 0xF0, 0x10, 0xF0 , // 5
+             0xF0, 0x80, 0xF0, 0x90, 0xF0 , // 6
+             0xF0, 0x10, 0x20, 0x40, 0x40 , // 7
+             0xF0, 0x90, 0xF0, 0x90, 0xF0 , // 8
+             0xF0, 0x90, 0xF0, 0x10, 0xF0 , // 9
+             0xF0, 0x90, 0xF0, 0x90, 0x90 , // A
+             0xE0, 0x90, 0xE0, 0x90, 0xE0 , // B
+             0xF0, 0x80, 0x80, 0x80, 0xF0 , // C
+             0xE0, 0x90, 0x90, 0x90, 0xE0 , // D
+             0xF0, 0x80, 0xF0, 0x80, 0xF0 , // E
+             0xF0, 0x80, 0xF0, 0x80, 0x80   // F
+        };
+        
         this.window = window;
         this.window.Render += OnRender;
         this.window.Load += Onload;
-
-        //load up memory
-        foreach(byte fontByte in Fonts)
-        {
-            memory.Append(fontByte);
-        }
-        foreach(byte instruction in instructions)
-        {
-            memory.Append(instruction);
-        }
+        memory.Append(fonts);
+        memory.Append(instructions);
     }
     
     public void Run()
@@ -225,7 +291,7 @@ public static class Program
 {
     private static void Main(string[] args)
     {
-        var chip8FilePath = Path.Join(Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName,"ibm_logo.ch8");
+        var chip8FilePath = Path.Join(Environment.CurrentDirectory, "Roms", "ibm_logo.ch8");
         byte[] data = File.ReadAllBytes(chip8FilePath);
 
         Chip8 chip8 = new Chip8.Builder()
--- Chip8Test/LearningTests.cs
+++ Chip8Test/LearningTests.cs
@@ -1,3 +1,19 @@
+// Memory: CHIP-8 has direct access to up to 4 kilobytes of RAM
+// Display: 64 x 32 pixels (or 128 x 64 for SUPER-CHIP) monochrome, ie. black or white
+// A program counter, often called just “PC”, which points at the current instruction in memory
+// One 16-bit index register called “I” which is used to point at locations in memory
+// A stack for 16-bit addresses, which is used to call subroutines/functions and return from them
+// An 8-bit delay timer which is decremented at a rate of 60 Hz (60 times per second) until it reaches 0
+// An 8-bit sound timer which functions like the delay timer, but which also gives off a beeping sound as long as it’s not 0
+// 16 8-bit (one byte) general-purpose variable registers numbered 0 through F hexadecimal, ie. 0 through 15 in decimal, called V0 through VF
+// VF is also used as a flag register; many instructions will set it to either 1 or 0 based on some rule, for example using it as a carry flag
+
+// each instruction is two bytes
+// chip 8 instruction layout (nibble = half a byte)
+// 
+// X: The second nibble. Used to look up one of the 16 registers (VX) from V0 through VF.
+// Y: The third nibble. Also used to look up one of the 16 registers (VY) from V0 through VF.
+// N: The fourth nibble. A 4-bit number.
 using System.Buffers.Binary;
 using NUnit.Framework.Constraints;
 
@@ -22,6 +38,7 @@ public class LearningTests
     }
 
 
+    // section 
     [TestCase("ibm_logo.ch8", 0, 1, 0x00E0)]
     [TestCase("ibm_logo.ch8", 2, 3, 0xA22A)]
     public void Chip8ByteToInstructionTest(string fileName, int startPosition, int endPosition, int expectedResult) // ensure I am reading data correctly
